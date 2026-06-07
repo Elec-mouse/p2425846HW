0bc333f531a2fd60d1629a12ac510634df9ed714
elec_mouse <3481343748@qq.com>
2026-06-07 18:21:12 +0800

    1、添加为base.html编写的CSS文件 2、把CSS文件加入静态资源 3、修改settings中静态资源路径配置

--- p2425846HW/urls.py
+++ p2425846HW/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('enrol/', include('enrol.urls')),
     path('', RedirectView.as_view(url='/enrol/')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
