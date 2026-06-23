66c770ef70f65d69f54cc33b1f20fe1722046115
Elec-mouse <76611361+Elec-mouse@users.noreply.github.com>
2026-06-24 00:51:03 +0800

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('enrol/', include('enrol.urls')),
+    path('', RedirectView.as_view(url='/enrol/')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
